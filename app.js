a7bb30c26b8407445174ed8999eb85b6ff38037b
bcosta1 <bcosta@stevens.edu>
2021-05-15 22:26:42 -0400

    it

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const static = express.static(__dirname + '/public');
-const flavors = require('./flavorsdummy');
-flavors.main();
 
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
